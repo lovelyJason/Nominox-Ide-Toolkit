f72cf1bbef70eb6117bbf38161abdc6aff1ef16d
GitHub Action <action@github.com>
2025-06-17 02:55:13 +0000

    Template cleanup

--- src/main/kotlin/com/github/lovelyjason/nominoxidetoolkit/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/lovelyjason/nominoxidetoolkit/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.lovelyjason.nominoxidetoolkit.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.lovelyjason.nominoxidetoolkit.MyBundle
+import com.github.lovelyjason.nominoxidetoolkit.services.MyProjectService
 import javax.swing.JButton
 
 
